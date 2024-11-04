38a331ae9cc0bbb0943074d8c7f30078b664b63b
Jakob Heuser <jakob@codedrift.com>
2024-11-04 13:56:17 -0800

    fix: inverts type check

--- source/10_homebrew.sh
+++ source/10_homebrew.sh
@@ -1,10 +1,10 @@
   # Try shell env until it works
-  if [[ ! "$(type -P /usr/local/bin/brew)" ]]; then
+  if [[ "$(type -P /usr/local/bin/brew)" ]]; then
     eval "$(/usr/local/bin/brew shellenv)"
   fi
-  if [[ ! "$(type -P /opt/homebrew/bin/brew)" ]]; then
+  if [[ "$(type -P /opt/homebrew/bin/brew)" ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
-  if [[ ! "$(type -P /home/linuxbrew/.linuxbrew/bin/brew)" ]]; then
+  if [[ "$(type -P /home/linuxbrew/.linuxbrew/bin/brew)" ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   fi
